abd743555d09c7f24d5a19a44647e555b8be326d
jethro tang <jthtang10@outlook.com>
2020-02-19 23:09:23 +0900

    template

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:create, :index, :new, :show] do
+        resources :reviews, only: [:new, :create]
+  end
 end
+
+  # get "restaurants/:restaurant_id/reviews",     to: "reviews#index"
+  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  # post "restaurants/:restaurant_id/reviews",    to: "reviews#create"
+  # get "reviews/:id",                            to: "reviews#show"
+  # get "reviews/:id/edit",                       to: "reviews#edit"
+  # patch "reviews/:id",                          to: "reviews#update"
+  # delete "reviews/:id",                         to: "reviews#destroy"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Duck & Waffle',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '123',
+    category:     'chinese'
+  },
+  {
+    name:         'Nobu',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '321',
+    category:     'japanese'
+  },
+  {
+    name:         'Dominique Ansel Bakery',
+    address:      '200A White City High St, London W10 6PQ',
+    phone_number: '654',
+    category:     'italian'
+  },
+  {
+    name:         'Pizza Hut',
+    address:      '500B High St, London E6 8PQ',
+    phone_number: '908',
+    category:     'french'
+  },
+  {
+    name:         'KFC',
+    address:      '99A Shoreditch High St, London E1 6PQ',
+    phone_number: '453',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
