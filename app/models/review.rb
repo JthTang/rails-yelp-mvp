a4c589c247f5f2d37eea49160d13c19acc77e92a
jethro tang <jthtang10@outlook.com>
2020-05-01 00:03:06 +0900

    updated yarn and gems

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,4 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
   validates :rating, numericality: { only_integer: true }, presence: true, inclusion: { in: (0..5) }
-  end
+end
